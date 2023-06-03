c72477a311ab3e822cedf9d1fd9a6625666fe130
Onurcpgl <onurcpgl.52@gmail.com>
2023-06-03 17:55:23 +0300

    delete client

--- client/src/Pages/Home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Home() {
-  return (
-    <div className="px-48 flex justify-center items-center text-5xl">Home</div>
-  )
-}
-
-export default Home
--- client/src/Pages/MyTeamTodo/MyTeam.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function MyTeam() {
-  return <div>MyTeam</div>;
-}
-
-export default MyTeam;
